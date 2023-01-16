b8b5ad734542c996c715ccab36d6e744aaa136c3
George Samuel <samuelamupitan827@gmail.com>
2023-01-16 13:20:26 +0100

    Update Footer.jsx

--- src/Component/footer/Footer.jsx
+++ src/Component/footer/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
     <footer>
       {/* <a href="" className='footer__logo'>Samuel</a> */}
       <ul className='permalinks'>
-        <li><a href="#">Home</a></li>
+        <li><a href="/">Home</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#ser">Services</a></li>
